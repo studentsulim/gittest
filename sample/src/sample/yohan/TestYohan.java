80e979c196df0581e8595f8f5f2d2fad7d1c0f1a
LG <LG@DESKTOP-AFLOV2G>
2024-04-18 17:31:14 +0900

    su_개인적으로

--- sample/src/sample/yohan/TestYohan.java
+++ sample/src/sample/yohan/TestYohan.java
@@ -1,11 +1,13 @@
 package sample.yohan;
 
 public class TestYohan {
+	private int age;
 	public void printName() {
 		System.out.println("강요한");
 		System.out.println("김동훈");
 		System.out.println("유선정");
 		System.out.println("김해진");
 		System.out.println("임성욱");
+		
 	}
 }
